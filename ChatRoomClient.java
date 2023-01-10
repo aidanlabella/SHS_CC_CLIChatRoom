1fe8121ee5510a0349a14e74319c84c602ca730c
aidanlabella <apl1341@rit.edu>
2023-01-09 20:16:02 -0500

    added client starter code

--- ChatRoomClient.java
+++ ChatRoomClient.java
@@ -6,38 +6,43 @@ public class ChatRoomClient {
     private static final String HOST = "localhost";
     private static final int PORT = 800;
 
-    Scanner scanner;
-    PrintStream printStream;
 
     public ChatRoomClient(String host, int port) {
         try {
             Socket socket = new Socket(host, port);
-            this.scanner = new Scanner(socket.getInputStream());
-            this.printStream = new PrintStream(socket.getOutputStream());
+
+            //TODO: PART 1: You will need to create an object for both text input and output
+            // hint: what is the type of System.in and System.out? Keep in mind these are both
+            // static objects that belong to the System class.
+            // See https://docs.oracle.com/javase/7/docs/api/java/lang/System.html
+
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
         }
     }
 
-    public void enter() {
-        if (scanner.next().equals(Protocol.OK)) {
-            printStream.println(Protocol.OK);
+    public void enterChatRoom() {
+         String firstMessage = getMessage();
+         if (firstMessage.equals(Protocol.OK)) {
+            // This is the "Handshake"
+            writeMessage(Protocol.OK);
 
-            printStream.println(Protocol.NAME);
-            printStream.println("Aidan");
+            //TODO: Part 4, this is where you will use the protocol to
+            //set your name, color and send a welcome message.
+            //You can use the last two lines where the "handshake" was
+            //completed as a refetence
+        }      
+    }
 
-            printStream.println(Protocol.COLOR);
-            printStream.println(Protocol.ANSI_CYAN);
-
-            printStream.println(Protocol.WELCOME);
-            printStream.println("Hi");
-        }
+    public String getMessage() {
+        //TODO: PART 3: use the class you found in PART 1 to get a message from the socket
+        return new String();
     }
 
     public void writeMessage(String string) {
-        this.printStream.println(Protocol.NEW_MSG);
-        this.printStream.println(string);
+        //TODO: PART 2: use the class you found in PART 1 to pass the string
+        // message to the server
     }
 
     /**
@@ -48,12 +53,10 @@ public class ChatRoomClient {
     public static void main(String [] args) {
         ChatRoomClient client = new ChatRoomClient(HOST, PORT);
         
-        client.enter();
+        client.enterChatRoom();
 
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            System.out.println("Enter next message:");
-            client.writeMessage(sc.nextLine());
-        }
+        //TODO: (Bonus) set up the chat room so that you can get text from the user (locally)
+        //and send it to the server. You may find the NEW_MSG constant in Protocol useful 
+        //for this.
     }
 }
